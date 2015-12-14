b419ffdbb8fb918381f16fb4c1421d046604cbc6
Derrick Jann <derrick.jann@sjsu.edu>
2015-12-13 17:58:34 -0800

    added custom styles

---
+++ README.md
@@ -0,0 +1,6 @@
+Sockets program: This program was done with by a tutorial  from a course by Andrew Mead. 
+
+
+
+
+Derrick Jann
--- server.js
+++ server.js
@@ -8,9 +8,15 @@ var app = express(); //calling express as a function
 
 var http = require('http').Server(app);//tells node to start new server and use this as a boiler plate
 
+var io=require('socket.io')(http); //calls http directly
 
 //expose folder
 app.use(express.static(__dirname + '/public'));
+
+
+io.on('connection', function() {
+	console.log('User connected via socket.io!');
+});//listen to events(connection event and run this fucntion)
 
 //start server with http
 
